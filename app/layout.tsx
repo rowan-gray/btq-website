20108bea6f4b2afa51a55a5defc7e9744992fdb7
Rowan Gray <github@rowangray.au>
2025-03-30 00:48:18 +1000

    Fixed production run not working properly and fixed font issues.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/tailwind.css'
 import type { Metadata } from 'next'
+import { Fira_Sans } from 'next/font/google'
 import NodeCache from 'node-cache'
 import RSSParser from 'rss-parser'
 
@@ -120,24 +121,20 @@ export const createPageMetadata = (params: {
   }
 }
 
+// Configure the Fira Sans font
+const firaSans = Fira_Sans({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // Include desired font weights
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="scrollbar-gutter-stable">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="bg-[#f1f1f1] font-sans text-gray-950 antialiased">
+      <body
+        className={`bg-[#f1f1f1] ${firaSans.className} text-gray-950 antialiased`}
+      >
         {children}
       </body>
     </html>
