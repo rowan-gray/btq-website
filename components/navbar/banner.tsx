0374a3add3f569aef89943bc8295f7b4fe7243e1
Rowan Gray <github@rowangray.au>
2025-12-14 10:33:20 +1000

    Fix <Link> elements not respecting intended hover state.

--- components/navbar/banner.tsx
+++ components/navbar/banner.tsx
@@ -14,7 +14,7 @@ export function Banner({ filled, text, href, expiry }: BannerProps) {
       {text && (expiry ? Date.now() < expiry.getTime() : true) && (
         <Link
           href={href}
-          className={`lg:text-md flex items-center justify-between gap-1 rounded-2xl py-0.5 pr-3 pl-4 text-sm/6 font-medium transition duration-200 ease-out ${filled ? 'bg-indigo-500 text-white data-[hover]:bg-indigo-600' : 'bg-indigo-300/35 text-black data-[hover]:bg-indigo-500/35'}`}
+          className={`lg:text-md flex items-center justify-between gap-1 rounded-2xl py-0.5 pr-3 pl-4 text-sm/6 font-medium transition duration-200 ease-out ${filled ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-indigo-300/35 text-black hover:bg-indigo-500/35'}`}
         >
           {text}
           <ChevronRightIcon className="size-4" />
--- components/navbar/desktop-navbar.tsx
+++ components/navbar/desktop-navbar.tsx
@@ -7,7 +7,7 @@ export function DesktopNavbar({ filled }: { filled: true | undefined }) {
       {NavLinks.map(({ href, label }) => (
         <Link
           href={href}
-          className={`flex h-[2rem] items-center px-4 text-base font-medium whitespace-nowrap transition duration-200 ease-in-out ${filled ? 'bg-indigo-500 text-white data-[hover]:bg-indigo-600' : 'bg-indigo-300/35 text-black data-[hover]:bg-indigo-500/35'} mr-1 rounded-xl`}
+          className={`flex h-[2rem] items-center px-4 text-base font-medium whitespace-nowrap transition duration-200 ease-in-out ${filled ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-indigo-300/35 text-black hover:bg-indigo-500/35'} mr-1 rounded-xl`}
           key={href}
         >
           {label}
